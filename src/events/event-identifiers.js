3cf9f6a5cbb2fdbee8a49225b6b783f9f3b05e5a
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2020-08-27 21:30:36 +0300

    Some Good Changes
    Object Decunstructing on command type
    
    AnyEvent event type
    
    bufferToConsoleString for buffer logging with custom logger
    
    Command now Extends the CommandData
    
    server.listen() port support server.listen([_port=19132])

--- src/events/event-identifiers.js
+++ src/events/event-identifiers.js
@@ -2,6 +2,7 @@ const EventIdentifiers = {
     PlayerJoinEvent: 'player_join',
     PlayerMoveEvent: 'player_move',
     PlayerChatEvent: 'player_chat',
-    PlayerInteractEvent: 'player_interact'
+    PlayerInteractEvent: 'player_interact',
+    AnyEvent: "*"
 }
 module.exports = EventIdentifiers
