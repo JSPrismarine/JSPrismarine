66597ad950effffbd0aafdb1d325f7d9c39a87e8
Daniel <danielweid2003@gmail.com>
2020-09-06 22:27:25 +0200

    Removed semicolons

--- src/command/vanilla/tell-command.js
+++ src/command/vanilla/tell-command.js
@@ -1,21 +1,21 @@
 /*
 Made with love by itsDkiller
-GH:       https://github.com/itsDkiller
+GH:      https://github.com/itsDkiller
 Discord: itsDkiller#4689
 
 */
 
-const Command = require('../command');
-const Player  = require('../../player');
-const Logger = require('../../utils/logger');
-const ConsoleSender = require('../console-sender');
+const Command = require('../command')
+const Player  = require('../../player')
+const Logger = require('../../utils/logger')
+const ConsoleSender = require('../console-sender')
 
 'use strict';
 
 class TellCommand extends Command {
 
     constructor() {
-        super({name: 'tell', description: 'Tell a message to a player.', aliases: ['msg']});
+        super({name: 'tell', description: 'Tell a message to a player.', aliases: ['msg']})
     }
 
     /**
@@ -24,44 +24,44 @@ class TellCommand extends Command {
      */
     execute(sender, args) {
 
-        let target;
-        let message;
+        let target
+        let message
 
         if (sender instanceof Player) {
 
             if (!args[0]) {
-                return sender.sendMessage('§cYou have to choose a player.');
+                return sender.sendMessage('§cYou have to choose a player.')
             } else if (!args[1]) {
-                return sender.sendMessage('§cCannot send an empty message.');
+                return sender.sendMessage('§cCannot send an empty message.')
             }
 
-            target = sender.getServer().getPlayerByName(args[0]);
-            message = args.slice(1).join(" ");
+            target = sender.getServer().getPlayerByName(args[0])
+            message = args.slice(1).join(" ")
 
-            if (!target) return sender.sendMessage('§cCannot find player ' + args[0] + '.');
-            if (target.name === sender.name) return sender.sendMessage('§cYou cannot send a message to yourself.');
+            if (!target) return sender.sendMessage('§cCannot find player ' + args[0] + '.')
+            if (target.name === sender.name) return sender.sendMessage('§cYou cannot send a message to yourself.')
 
-            target.sendMessage(`§e[${sender.name} -> ${target.name}]§r ${message}`);
-            sender.sendMessage(`§e[${sender.name} -> ${target.name}]§r ${message}`);
+            target.sendMessage(`§e[${sender.name} -> ${target.name}]§r ${message}`)
+            sender.sendMessage(`§e[${sender.name} -> ${target.name}]§r ${message}`)
 
         } else {
 
             if (!args[0]) {
-                return Logger.warn('You have to choose a player.');
+                return Logger.warn('You have to choose a player.')
             } else if (!args[1]) {
-                return Logger.warn('Cannot send an empty message.');
+                return Logger.warn('Cannot send an empty message.')
             }
 
-            target = sender.getServer().getPlayerByName(args[0]);
-            message = args.slice(1).join(" ");
+            target = sender.getServer().getPlayerByName(args[0])
+            message = args.slice(1).join(" ")
 
-            if (!target) return Logger.warn('Cannot find player ' + args[0] + '.');
+            if (!target) return Logger.warn('Cannot find player ' + args[0] + '.')
 
-            target.sendMessage(`§e[CONSOLE -> ${target.name}]§r ${message}`);
-            Logger.info(`[CONSOLE -> ${target.name}] ${message}`);
+            target.sendMessage(`§e[CONSOLE -> ${target.name}]§r ${message}`)
+            Logger.info(`[CONSOLE -> ${target.name}] ${message}`)
 
         }
     }
 }
 
-module.exports = TellCommand;
+module.exports = TellCommand
