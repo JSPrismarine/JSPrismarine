4352112cf0f1c952582c3fe1918699e07cc6ebaa
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2020-09-07 13:15:06 +0300

    finnally reverted the text handeler

--- src/network/handler/text-handler.js
+++ src/network/handler/text-handler.js
@@ -1,19 +1,34 @@
 const Player = require('../../player')
 const Identifiers = require('../identifiers')
-const SetTitlePacket = require('../packet/set-title-packet')
+const TextPacket = require('../packet/text')
+const EventManager = require('../../events/event-manager')
+const logger = require('../../utils/logger')
+const PlayerChatEvent = require('../../events/player/player-chat-event')
+const TextType = require('../type/text-type')
 
 'use strict'
 
-class SetTitleHandler {
-    static NetID = Identifiers.SetTitlePacket
+class TextHandler {
+    static NetID = Identifiers.TextPacket
 
     /**
-     * @param {SetTitlePacket} packet 
+     * @param {TextPacket} packet 
      * @param {Player} player 
      */
     static handle(packet, player) {
-        
+        let event = new PlayerChatEvent(this, packet.message)
+        EventManager.emit('player_chat', event)
+        if (event.isCancelled()) return
+
+        let vanillaFormat = `<${packet.sourceName}> ${event.getMessage()}`
+        logger.silly(vanillaFormat)
+
+        // Broadcast chat message to every player
+        if (packet.type == TextType.Chat) {
+            for (const [_, onlinePlayer] of player.getServer().players) {
+                onlinePlayer.sendMessage(vanillaFormat, packet.xuid)
+            }
+        }
     }
 }
-module.exports = SetTitleHandler
-
+module.exports = TextHandler
