afa5ebfb62463d0b54b2467bb411eaee47ab5988
Daniel <danielweid2003@gmail.com>
2020-09-14 17:42:12 +0200

    Removed package as a variable bcs of strict mode, added forgotten command class registries

--- src/command/vanilla/version-command.js
+++ src/command/vanilla/version-command.js
@@ -5,7 +5,7 @@ This command is non-vanilla, but exists in PMMP, Altay, MiNET, Nukkit a.s.o.
 const Command       = require('../command')
 const Player        = require('../../player')
 const ConsoleSender = require('../console-sender')
-const package       = require('../../../package.json')
+const packageFile   = require('../../../package.json')
 const identifiers   = require('../../network/identifiers')
 
 class VersionCommand extends Command {
@@ -19,7 +19,7 @@ class VersionCommand extends Command {
      * @param {Array} args 
      */
     execute(sender, args) {
-        let serverVersion    = package.version               //The only version reference I found, please contribute if there is a "better" one
+        let serverVersion    = packageFile.version               //The only version reference I found, please contribute if there is a "better" one
         let protocolVersion  = identifiers.Protocol
         let minecraftVersion = identifiers.MinecraftVersion
 
