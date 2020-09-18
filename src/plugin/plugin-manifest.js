5ba3f828d86607b14573d0f17bb89497f58ab8c4
HerryYT <enricoangelon.ea@gmail.com>
2020-09-18 14:26:09 +0200

    Adapted PR code to JSP standards

---
+++ src/plugin/plugin-manifest.js
@@ -0,0 +1,21 @@
+'use strict'
+
+class PluginManifest {
+
+    /** @type {string} */
+    name
+
+    /** @type {string} */
+    indexFile
+
+    /** @type {string} */
+    description
+
+    /** @type {string} */
+    version
+
+    /** @type {string} */
+    apiVersion
+    
+}
+module.exports = PluginManifest
---
+++ src/plugin/plugin.js
@@ -0,0 +1,18 @@
+const Prismarine = require('../prismarine')
+const PluginManifest = require('./plugin-manifest')
+
+'use strict'
+
+class Plugin {
+
+    /** @type {PluginManifest} */
+    manifest
+
+    /** @type {function(Prismarine)} */
+    main
+
+    /** @type {string} */
+    path
+    
+}
+module.exports = Plugin
