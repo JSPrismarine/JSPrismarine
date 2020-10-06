6233252e03589b5b2de29495e391eaac1d7e5204
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-06 21:46:12 +0200

    Use @jsprismarine/bedrock-data.

--- src/network/packet/available-actor-identifiers.js
+++ src/network/packet/available-actor-identifiers.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 const DataPacket = require('./packet')
 const Identifiers = require('../identifiers')
-const EntityIdentifiers = require('@filiphsandstrom/bedrock-data').entity_identifiers
+const EntityIdentifiers = require('@jsprismarine/bedrock-data').entity_identifiers
 
 'use strict'
 
@@ -12,7 +12,7 @@ class AvailableActorIdentifiersPacket extends DataPacket {
     #cachedNBT
 
     async encodePayload() {
-        this.append(this.#cachedNBT || (this.#cachedNBT = fs.readFileSync(__dirname + '/../../../node_modules/@filiphsandstrom/bedrock-data/resources/entity_identifiers.nbt')))
+        this.append(this.#cachedNBT || (this.#cachedNBT = fs.readFileSync(__dirname + '/../../../node_modules/@jsprismarine/bedrock-data/resources/entity_identifiers.nbt')))
     }
 }
 module.exports = AvailableActorIdentifiersPacket
