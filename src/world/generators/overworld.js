aa453170c9329f7ee1e422a7062e45a613f59b6e
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-08 00:45:23 +0200

    Start work on perlin noise.

--- src/world/generators/overworld.js
+++ src/world/generators/overworld.js
@@ -1,17 +1,21 @@
 'use strict'
+const Noise = require('simplex-noise')
 const Chunk = require('../chunk/chunk')
 
 class Overworld {
-    getChunk({ chunkX, chunkY, chunkZ, seed }) {
+    getChunk({ chunkX, chunkY = 0, chunkZ, seed }) {
+        const noise = new Noise(seed)
         const chunk = new Chunk(chunkX, chunkZ)
 
         for (let x = 0; x < 16; x++) {
             for (let z = 0; z < 16; z++) {
-                let y = 0
-                chunk.setBlockId(x, y++, z, 7)
-                chunk.setBlockId(x, y++, z, 3)
-                chunk.setBlockId(x, y++, z, 3)
-                chunk.setBlockId(x, y, z, 2)
+                const height = parseInt(`${noise.noise2D(chunkX + x, chunkZ + z) * 10}`)
+                for (let y = 0; y <= height; y++) {
+                    if (y === height)
+                        chunk.setBlockId(x, y, z, 2)
+                    else
+                        chunk.setBlockId(x, y, z, 3)
+                }
             }
         }
 
