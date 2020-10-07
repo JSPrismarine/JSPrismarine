45c8959b192a1fd5455006b49786013c95e2c5cf
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-07 14:20:07 +0200

    Add PacketViolationWarningHandler.

---
+++ src/network/handler/packet-violation-warning-handler.js
@@ -0,0 +1,16 @@
+const Identifiers = require('../identifiers')
+const PacketViolationWarningPacket = require('../packet/packet-violation-warning')
+
+class PacketViolationWarningHandler {
+    static NetID = Identifiers.PacketViolationWarningPacket
+
+    /**
+     * @param {PacketViolationWarningPacket} packet 
+     * @param {Prismarine} server
+     * @param {Player} player 
+     */
+    static handle(packet, server, player) {
+        server.getLogger().error(`Packet violation: ${JSON.stringify(packet)}`)
+    }
+}
+module.exports = PacketViolationWarningHandler
