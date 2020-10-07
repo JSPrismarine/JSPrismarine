36aa63fd7b2e4cf61028d50a03d7daab31902ee9
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-07 16:33:48 +0200

    Add teleport command.

---
+++ src/command/vanilla/tp-command.js
@@ -0,0 +1,37 @@
+const Command = require('../command')
+const Player = require('../../player')
+const logger = require('../../utils/logger')
+const ConsoleSender = require('../console-sender')
+const Gamemode = require('../../world/gamemode')
+
+'use strict'
+
+class GamemodeCommand extends Command {
+
+    constructor() {
+        super({ namespace: 'minecraft', name: 'tp', description: 'Teleports a player to a specified location' })
+    }
+
+    /**
+     * @param {ConsoleSender|Player} sender 
+     * @param {Array} args
+     */
+    execute(sender, args) {
+        if (args.length !== 4) {
+            return sender.sendMessage('§cYou have to specify <user> x y z.')
+        }
+
+        // TODO: handle only supplying x y, and relative teleport
+
+        const target = sender.getServer().getPlayerByName(args[0])
+        if (!target)
+            return sender.sendMessage(`§c${args[0]} is not online!`)
+
+        target.x = args[1]
+        target.y = args[2]
+        target.z = args[3]
+        target.broadcastMove(target)
+        sender.sendMessage(`Teleported ${args[0]} to ${target.x} ${target.y} ${target.z}`)
+    }
+}
+module.exports = GamemodeCommand
