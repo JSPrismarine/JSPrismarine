b04a125c89943e350a06cca6c770d78ac80b57cb
HerryYT <enricoangelon.ea@gmail.com>
2020-10-07 21:44:20 +0200

    Added ItemStackRequest packet stub

---
+++ src/network/packet/item-stack-request.js
@@ -0,0 +1,17 @@
+const DataPacket = require('./packet')
+const Identifiers = require('../identifiers')
+
+'use strict'
+
+class ItemStackRequestPacket extends DataPacket {
+    static NetID = Identifiers.ItemStackRequestPacket
+
+    requests = []
+
+    decodePayload() {
+        for (let i = 0; i < this.readUnsignedVarInt(); i++) {
+            this.requests.push(this.readItemStackRequest())
+        }
+    }
+}
+module.exports = ItemStackRequestPacket
