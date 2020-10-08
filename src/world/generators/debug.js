f0abd83105b58738a74c0091dc6a5e6303b9d0bd
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-08 10:48:27 +0200

    Add debug generator.

---
+++ src/world/generators/debug.js
@@ -0,0 +1,18 @@
+'use strict';
+const Chunk = require('../chunk/chunk');
+
+class Debug {
+    getChunk({ chunkX, chunkY, chunkZ, seed }) {
+        const chunk = new Chunk(chunkX, chunkZ);
+
+        for (let x = 0; x < 16; x++) {
+            for (let z = 0; z < 16; z++) {
+                chunk.setBlockId(x, 1, z, chunkX * 16 + x);
+                chunk.setBlockId(x, 0, z, 7);
+            }
+        }
+
+        return chunk;
+    }
+}
+module.exports = Debug;
