399f35430000ef9b926ca0dc075f9e028859f5e8
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-14 22:08:33 +0200

    Add CoarseDirt.

--- src/block/blocks/Dirt.ts
+++ src/block/blocks/Dirt.ts
@@ -2,13 +2,19 @@ import Solid from '../Solid';
 import { BlockIdsType } from '../BlockIdsType';
 import { BlockToolType } from '../BlockToolType';
 
-export default class Grass extends Solid {
-    constructor() {
+export enum DirtType {
+    Regular = 0,
+    Coarse = 1
+};
+
+export default class Dirt extends Solid {
+    constructor(name: string = 'minecraft:dirt', type: DirtType = DirtType.Regular) {
         super({
-            name: 'minecraft:dirt',
+            name: name,
             id: BlockIdsType.Dirt,
-            hardness: 0.5
+            hardness: 0.5,
         });
+        this.meta = type;
     }
 
     getToolType() {
