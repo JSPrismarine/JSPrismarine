6bbaa8c091caeb476d48b9a583f0668172acf6c5
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-19 00:25:43 +0200

    Resolve #97.

--- src/command/jsprismarine/VersionCommand.ts
+++ src/command/jsprismarine/VersionCommand.ts
@@ -1,9 +1,6 @@
-/*
-This command is non-vanilla, but exists in PMMP, Altay, MiNET, Nukkit a.s.o.
-*/
-
 import Player from "../../player";
 import Command from "../";
+import git from 'git-rev-sync';
 
 const packageFile = require('../../../package.json');
 const identifiers = require('../../network/identifiers');
@@ -22,6 +19,6 @@ export default class VersionCommand extends Command {
         let protocolVersion = identifiers.Protocol;
         let minecraftVersion = identifiers.MinecraftVersion;
 
-        sender.sendMessage(`This server is running on JSPrismarine ${serverVersion} for Minecraft: Bedrock Edition v${minecraftVersion} (protocol version ${protocolVersion})`);
+        sender.sendMessage(`This server is running on JSPrismarine ${serverVersion} (rev-${git.short() || 'unknown'}) for Minecraft: Bedrock Edition v${minecraftVersion} (protocol version ${protocolVersion})`);
     }
 }
--- src/query/QueryManager.ts
+++ src/query/QueryManager.ts
@@ -1,6 +1,7 @@
 import Prismarine from "../prismarine";
 import BinaryStream from "@jsprismarine/jsbinaryutils";
 import udp from 'dgram';
+import git from 'git-rev-sync';
 
 export default class QueryManager {
     private server?: udp.Socket;
@@ -9,8 +10,8 @@ export default class QueryManager {
         if (!server.getConfig().getEnableQuery())
             return;
 
-        // TODO: setup query
         const port = server.getConfig().getQueryPort()
+        const git_rev = git.short() || 'unknown';
         this.server = udp.createSocket('udp4');
 
         // TODO: https://wiki.vg/Server_List_Ping
@@ -18,7 +19,7 @@ export default class QueryManager {
             const buffer = new BinaryStream(Buffer.from(data.buffer))
             const magic = buffer.readShort();
             const type = buffer.readByte();
-            const sessionId = buffer.readInt();
+            const sessionId = buffer.readInt() & 0x0F0F0F0F;
 
             if (magic !== 65277)
                 return server.getLogger().silly(`Query ${magic} !== 65277. ${JSON.stringify(info)}`);
@@ -27,16 +28,17 @@ export default class QueryManager {
                 case 0: { // Stats
                     const padding = buffer.readRemaining();
                     if (padding.length < 8) {
-                        // TODO: use bonary stream
-                        let res = Buffer.alloc(5);
-                        res.writeUInt8(0);
-                        res.writeInt32BE(0);
-                        res = Buffer.concat([res, Buffer.from(`${server.getRaknet().name.getMotd()}\0`, 'binary')]);
-                        res = Buffer.concat([res, Buffer.from('SMP\0', 'binary')]);
-                        res = Buffer.concat([res, Buffer.from(`${server.getWorldManager().getDefaultWorld()?.getName()}\0`, 'binary')]);
-                        res = Buffer.concat([res, Buffer.from(`${server.getRaknet().name.getOnlinePlayerCount()}\0`, 'binary')]);
-                        res = Buffer.concat([res, Buffer.from(`${server.getRaknet().name.getMaxPlayerCount()}\0`, 'binary')]);
-                        this.server?.send(res, info.port, info.address);
+                        const res = new BinaryStream();
+                        res.writeByte(0);
+                        res.writeInt(sessionId);
+                        res.append(Buffer.from(`${[
+                            server.getRaknet().name.getMotd(),
+                            'SMP',
+                            server.getWorldManager().getDefaultWorld()?.getName(),
+                            server.getRaknet().name.getOnlinePlayerCount(),
+                            server.getRaknet().name.getMaxPlayerCount(),
+                        ].join('\0')}\0`, 'binary'));
+                        this.server?.send(res.getBuffer(), info.port, info.address);
                     } else {
                         const res = new BinaryStream();
                         res.writeByte(0);
@@ -52,8 +54,8 @@ export default class QueryManager {
                         res.writeByte(0);
                         res.writeByte(128);
                         res.writeByte(0);
-
                         // End padding
+
                         res.append(Buffer.from(`\0${[
                             'hostname',
                             server.getRaknet().name.getMotd(),
@@ -62,7 +64,7 @@ export default class QueryManager {
                             'game_id',
                             'MINECRAFT',
                             'version',
-                            '', // TODO
+                            `${server.getConfig().getVersion()}:${git_rev}`,
                             'plugins',
                             'JSPrismarine:', // TODO
                             'map',
@@ -95,12 +97,11 @@ export default class QueryManager {
                     }
                     break;
                 } case 9: { // Handshake
-                    // TODO: use bonary stream
-                    const buffer = Buffer.alloc(7);
-                    buffer.writeUInt8(9);
-                    buffer.writeInt32BE(sessionId);
-                    buffer.writeInt32BE(9513307);
-                    this.server?.send(buffer, info.port, info.address);
+                    const res = new BinaryStream();
+                    res.writeByte(9);
+                    res.writeInt(sessionId);
+                    res.append(Buffer.from(`9513307\0`, 'binary'));
+                    this.server?.send(res.getBuffer(), info.port, info.address);
                     break;
                 }
             };
