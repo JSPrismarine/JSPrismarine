7754750fef0df7bf63cb30dc377b58870d9856df
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-20 04:55:35 +0200

    Add tps command.

---
+++ src/command/jsprismarine/TpsCommand.ts
@@ -0,0 +1,15 @@
+import Player from "../../player";
+import PluginFile from "../../plugin/PluginFile";
+
+const Command = require('../').default;
+
+export default class PluginsCommand extends Command {
+    constructor() {
+        super({ id: 'jsprismarine:tps', description: 'Get current TPS' });
+    }
+
+    public execute(sender: Player, args: Array<any>): void {
+        sender.sendMessage(`TPS: §e${sender.getServer().getTPS()}`);
+        return;
+    }
+}
