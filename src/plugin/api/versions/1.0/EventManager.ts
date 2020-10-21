5731d1027378c020a2635ea849fd10fa7674373f
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-21 16:20:40 +0200

    Fix PluginApi naming.

--- src/plugin/api/versions/1.0/EventManager.ts
+++ src/plugin/api/versions/1.0/EventManager.ts
@@ -1,5 +1,6 @@
 import type Prismarine from "../../../../Prismarine";
 
+// TODO: create an event forwarder, plugins shouldn't directly access internal classes such as the Player, World etc.
 export default class EventManager {
     private server;
 
@@ -10,5 +11,7 @@ export default class EventManager {
     public async on(id: string, callback: any) {
         return await this.server.getEventManager().on(id, callback);
     }
-    public async emit(id: string, value: any) {}
+    public async emit(id: string, value: any) {
+        return await this.server.getEventManager().emit(id, value);
+    }
 };
