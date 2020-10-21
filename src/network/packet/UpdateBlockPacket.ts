1cc2345c18488a39fdad21733ea1894c0aae1dcd
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-21 20:24:06 +0200

    Fix UpdateBlockPacket.

---
+++ src/network/packet/UpdateBlockPacket.ts
@@ -0,0 +1,32 @@
+import Identifiers from "../identifiers";
+import DataPacket from "./packet";
+
+export enum UpdateBlockLayerType {
+    Normal = 0,
+    Liquid = 1
+};
+
+export enum UpdateBlockFlagsType {
+    None = 0b0000
+};
+
+export default class UpdateBlockPacket extends DataPacket {
+    static NetID = Identifiers.UpdateBlockPacket
+
+    x : number= 0.0;
+    y: number = 0;
+    z: number = 0;
+    BlockRuntimeId: number = 0;
+    Flags: UpdateBlockFlagsType = UpdateBlockFlagsType.None;
+    Layer: UpdateBlockLayerType = UpdateBlockLayerType.Normal;
+
+    encodePayload() {
+        this.writeVarInt(this.x);
+        this.writeUnsignedVarInt(this.y);
+        this.writeVarInt(this.z);
+
+        this.writeUnsignedVarInt(this.BlockRuntimeId);
+        this.writeUnsignedVarInt(this.Flags);
+        this.writeUnsignedVarInt(this.Layer);
+    }
+}
