0ea87d1884032fde29ae2b54a2c78c81ed971d5d
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-29 15:13:22 +0100

    Add AdventureSettingsPacket decoding & encoding.

---
+++ src/network/handler/AdventureSettingsHandler.ts
@@ -0,0 +1,11 @@
+import type Player from "../../player/Player";
+import type Prismarine from "../../Prismarine";
+import type AdventureSettingsPacket from "../packet/AdventureSettingsPacket";
+
+const Identifiers = require('../Identifiers').default;
+
+export default class AdventureSettingsHandler {
+    static NetID = Identifiers.AdventureSettingsPacket
+
+    static handle(packet: AdventureSettingsPacket, server: Prismarine, player: Player) { }
+};
