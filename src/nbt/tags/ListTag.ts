3aa3b88c84aadaa03a1d60354083510e11626e65
HerryYT <enricoangelon.ea@gmail.com>
2020-11-05 13:07:56 +0100

    Update ListTag.ts

--- src/nbt/tags/ListTag.ts
+++ src/nbt/tags/ListTag.ts
@@ -3,6 +3,7 @@ import TagType from './internal/TagType';
 
 export default class ListTag extends Tag {
     public tagType: TagType; 
+    public type = TagType.List; 
 
     constructor(value: (Tag|null)[], name: string, tagType = TagType.End) {
         super(value, name);
