4a88dea05bb207879517a0ebddc2e3cea9aad1a5
greyliances <36825953+greyliances@users.noreply.github.com>
2020-11-05 18:10:42 +0100

    Added Missing BlockIds, some new Blocks and fixed Planks
    - All IDs that have been in  https://github.com/pmmp/BedrockData/blob/afc885cccae38048d309911f2c5ddcdcb6af8152/block_id_map.json were added to the BlockIdsType Enum.
    
    The names were adjusted to match with the Naming Convention
    
    Info: DISPLAYING any Block with a legacy ID above 255 in the Inventory needs its ItemIdsType
    
    Here is an example with Crying Obsidian
    
    ItemID:
    "minecraft:crying_obsidian": -289
    The ItemID is what's needed to have the block in the Inventory
    
    BlockID:
    "minecraft:crying_obsidian": 544
    Apparently this is the legacy block ID
    
    - Added Andesite
    - Added Polished Andesite
    - Added Glass, and all Stained Glass Variants
    - Added Gravel
    - Added Flint
    - Added Lapis Ore
    - Added Lapis Block
    - Added Sponge
    - Added Wet Sponge
    - Fixed Typo in inventory
    - Fixed Jungle Planks mishap. They were declared to be Birch Planks
    - Oak Planks are now properly added to the inventory

---
+++ src/block/blocks/RedStainedGlass.ts
@@ -0,0 +1,7 @@
+import StainedGlass, { StainedGlassType } from "./StainedGlass";
+
+export default class RedStainedGlass extends StainedGlass {
+	constructor() {
+		super("minecraft:red_stained_glass", StainedGlassType.Red);
+	}
+}
