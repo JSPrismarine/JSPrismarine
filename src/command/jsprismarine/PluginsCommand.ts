1bbab53393fa84a6c839962cda8d91f0d8b2326b
Tobi <39420707+TobidieTopfpflanze@users.noreply.github.com>
2020-11-12 03:13:39 +0100

    expand teleport command and add search to plugins command
    tpsCommand => change require to import

--- src/command/jsprismarine/PluginsCommand.ts
+++ src/command/jsprismarine/PluginsCommand.ts
@@ -1,7 +1,9 @@
+import CommandParameter, {
+    CommandParameterType
+} from '../../network/type/CommandParameter';
 import Player from '../../player/Player';
 import PluginFile from '../../plugin/PluginFile';
-
-const Command = require('../Command').default;
+import Command from '../Command';
 
 export default class PluginsCommand extends Command {
     constructor() {
@@ -10,21 +12,43 @@ export default class PluginsCommand extends Command {
             description: 'Lists all plugins that run on the server.',
             aliases: ['pl']
         });
+
+        this.parameters = [new Set()];
+
+        this.parameters[0].add(
+            new CommandParameter({
+                name: 'plugin',
+                type: CommandParameterType.String,
+                optional: true
+            })
+        );
     }
 
-    /**
-     * @param {Player} sender
-     * @param {Array} args
-     */
-    public execute(sender: Player, args: Array<any>): void {
+    public execute(sender: Player, args: Array<string>): void {
         let plugins = sender.getServer().getPluginManager().getPlugins();
+        let message: string = '';
 
         if (plugins.length == 0) {
             sender.sendMessage("§cCan't find any plugins.");
             return;
         }
 
-        let message = `§7Plugins (${plugins.length}):§r ${plugins
+        if (args[0]) {
+            plugins.filter((plugin) =>
+                plugin.getDisplayName().includes(args[0])
+            );
+            if (plugins.length == 0) {
+                sender.sendMessage(
+                    `§cCan't find any plugins for '${args[0]}'.`
+                );
+                return;
+            }
+            message += `$7 Plugins found for '${args[0]}' (${plugins.length}): `;
+        } else {
+            message += `§7Plugins (${plugins.length}): `;
+        }
+
+        message += `§r ${plugins
             .map((p: PluginFile) => `§a${p.getDisplayName()} ${p.getVersion()}`)
             .join('§r, ')}`;
 
