a07696742cf1d0c47581d1f522bedac39d85416d
HerryYT <enricoangelon.ea@gmail.com>
2020-11-14 14:34:56 +0100

    Implemented writing

--- src/nbt/NBTReader.ts
+++ src/nbt/NBTReader.ts
@@ -13,7 +13,7 @@ export default class NBTReader extends NBTStreamReader {
 
     public parseList(): Set<any> {
         this.expectInput(this.isUsingVarint() ? 2 : 3, 'Invalid NBT Data: Not enough data to read new tag', false);
-        if (this.readByteValue() != NBTDefinitions.TAG_LIST) {
+        if (this.readByteValue().getValue() != NBTDefinitions.TAG_LIST) {
             throw new Error('Invalid NBT Data: Not list tag found');
         }
 
@@ -23,11 +23,11 @@ export default class NBTReader extends NBTStreamReader {
 
     public parse(): NBTTagCompound {
         this.expectInput(this.isUsingVarint() ? 2 : 3, 'Invalid NBT Data: Not enough data to read new tag', false);
-        if (this.readByteValue() != NBTDefinitions.TAG_COMPOUND) {
+        if (this.readByteValue().getValue() != NBTDefinitions.TAG_COMPOUND) {
             throw new Error('Invalid NBT Data: No root tag found');
         }
 
-        let name: string = this.readStringValue();
+        let name: string = this.readStringValue().getValue();
         let root: NBTTagCompound = this.readTagCompoundValue();
         root.setName(name);
         return root;
@@ -38,51 +38,51 @@ export default class NBTReader extends NBTStreamReader {
         let compound: NBTTagCompound = new NBTTagCompound();
         this.expectInput(1, 'Invalid NBT Data: Expected Tag ID in compound tag', false);
 
-        let tagID = this.readByteValue();
+        let tagID = this.readByteValue().getValue();
         while (tagID != NBTDefinitions.TAG_END) {
             switch (tagID) {
                 case NBTDefinitions.TAG_BYTE:
-                    compound.addValue(this.readStringValue(), this.readByteValue());
+                    compound.addValue(this.readStringValue().getValue(), this.readByteValue());
                     break;
                 case NBTDefinitions.TAG_SHORT:
-                    compound.addValue(this.readStringValue(), this.readShortValue());
+                    compound.addValue(this.readStringValue().getValue(), this.readShortValue());
                     break;
                 case NBTDefinitions.TAG_INT:
-                    compound.addValue(this.readStringValue(), this.readIntValue());
+                    compound.addValue(this.readStringValue().getValue(), this.readIntValue());
                     break;
                 case NBTDefinitions.TAG_LONG:
-                    compound.addValue(this.readStringValue(), this.readLongValue());
+                    compound.addValue(this.readStringValue().getValue(), this.readLongValue());
                     break;
                 case NBTDefinitions.TAG_FLOAT:
-                    compound.addValue(this.readStringValue(), this.readFloatValue());
+                    compound.addValue(this.readStringValue().getValue(), this.readFloatValue());
                     break;
                 case NBTDefinitions.TAG_DOUBLE:
-                    compound.addValue(this.readStringValue(), this.readDoubleValue());
+                    compound.addValue(this.readStringValue().getValue(), this.readDoubleValue());
                     break;
                 case NBTDefinitions.TAG_BYTE_ARRAY:
-                    compound.addValue(this.readStringValue(), this.readByteArrayValue());
+                    compound.addValue(this.readStringValue().getValue(), this.readByteArrayValue());
                     break;
                 case NBTDefinitions.TAG_STRING:
-                    compound.addValue(this.readStringValue(), this.readStringValue());
+                    compound.addValue(this.readStringValue().getValue(), this.readStringValue());
                     break;
                 case NBTDefinitions.TAG_LIST:
-                    compound.addValue(this.readStringValue(), this.readTagListValue());
+                    compound.addValue(this.readStringValue().getValue(), this.readTagListValue());
                     break;
                 case NBTDefinitions.TAG_COMPOUND:
-                        let name: string = this.readStringValue();
+                        let name: string = this.readStringValue().getValue();
                         let child: NBTTagCompound = this.readTagCompoundValue();
                         child.setName(name);
                         compound.addChild(child);
                     break;
                 case NBTDefinitions.TAG_INT_ARRAY:
-                    compound.addValue(this.readStringValue(), this.readIntArrayValue());
+                    compound.addValue(this.readStringValue().getValue(), this.readIntArrayValue());
                     break;
                 default:
                     throw new Error(`Invalid NBT Data: Unknown tag <${tagID}>`);
             }
             
             this.expectInput(1, 'Invalid NBT Data: Expected tag ID in tag compound', false);
-            tagID = this.readByteValue();
+            tagID = this.readByteValue().getValue();
         }
 
         return compound;
@@ -90,8 +90,8 @@ export default class NBTReader extends NBTStreamReader {
 
     private readTagListValue(): Set<any> {
         this.expectInput(this.isUsingVarint() ? 2 : 5, 'Invalid NBT Data: Expected TAGList header', false);
-        let listType: number = this.readByteValue();
-        let listLength: number = this.readIntValue();
+        let listType: number = this.readByteValue().getValue();
+        let listLength: number = this.readIntValue().getValue();
 
         this.alterAllocationLimit(Allocation.ARRAY_LIST);
         this.alterAllocationLimit(Allocation.REFERENCE);
--- src/nbt/NBTWriter.ts
+++ src/nbt/NBTWriter.ts
@@ -1,6 +1,14 @@
 import BinaryStream from "@jsprismarine/jsbinaryutils";
 import { ByteOrder } from "./ByteOrder";
 import { NBTDefinitions } from "./NBTDefinitions";
+import NBTTagCompound from "./NBTTagCompound";
+import ByteVal from "./types/ByteVal";
+import DoubleVal from "./types/DoubleVal";
+import FloatVal from "./types/FloatVal";
+import LongVal from "./types/LongVal";
+import NumberVal from "./types/NumberVal";
+import ShortVal from "./types/ShortVal";
+import StringVal from "./types/StringVal";
 
 export default class NBTWriter {
     private readonly MAX_SIZE = 10 * 1024 * 1024;
@@ -24,6 +32,11 @@ export default class NBTWriter {
         this.writeListValue(list);
     }
 
+    public writeCompound(compound: NBTTagCompound): void {
+        this.writeTagHeader(NBTDefinitions.TAG_COMPOUND, compound.getName() ?? '');
+        this.writeCompoundValue(compound);
+    }
+
     private writeTagHeader(type: number, name: string): void {
         this.writeByteValue(type);
         this.writeStringValue(name);
@@ -38,6 +51,188 @@ export default class NBTWriter {
         }
 
         this.buf.append(bytes); 
+    }
+
+    public writeByteValue(value: number): void {
+        this.buf.writeByte(value);
+    }
+
+    public writeShortValue(value: number): void {
+        if (this.order == ByteOrder.LITTLE_ENDIAN) {
+            this.buf.writeLShort(value);
+        } else {
+            this.buf.writeShort(value);
+        }
+    }
+
+    private writeIntegerValue(value: number): void {
+        if (this.useVarint) {
+            this.buf.writeUnsignedVarInt(value);
+        } else {
+            if (this.order == ByteOrder.LITTLE_ENDIAN) {
+                this.buf.writeLInt(value);
+            } else {
+                this.buf.writeInt(value);
+            }
+        }
+    }
+
+    private writeLongValue(value: bigint): void {
+        if (this.useVarint) {
+            this.buf.writeVarLong(value);
+        } else {
+            if (this.order == ByteOrder.LITTLE_ENDIAN) {
+                this.buf.writeLLong(value);
+            } else {
+                this.buf.writeLong(value);
+            }
+        }
+    }
+
+    private writeFloatValue(value: number): void {
+        if (this.order == ByteOrder.LITTLE_ENDIAN) {
+            this.buf.writeLFloat(value);
+        } else {
+            this.buf.writeFloat(value);
+        }
+    }
+
+    private writeDoubleValue(value: number): void {
+        if (this.order == ByteOrder.LITTLE_ENDIAN) {
+            this.buf.writeLDouble(value);
+        } else {
+            this.buf.writeDouble(value);
+        }
+    }
+
+    private writeByteArrayValue(value: Buffer): void {
+        this.writeIntegerValue(value.length);
+        this.buf.append(value);
+    }
+
+    private writeIntegerArrayValue(value: number[]) {
+        this.writeIntegerValue(value.length);
+        value.map(v => this.writeIntegerValue(v));
+    }
+
+    private writeListValue(value: Set<any>): void {
+        if (value.size > 0) {
+            let listNbtType = this.getNBTTypeFromValue(Array.from(value)[0]);
+            this.writeByteValue(listNbtType);
+            this.writeIntegerValue(value.size);
+            for (let rawValue of value) {
+                switch (listNbtType) {
+                    case NBTDefinitions.TAG_BYTE:
+                        this.writeByteValue(rawValue.getValue());
+                        break;
+                    case NBTDefinitions.TAG_SHORT:
+                        this.writeShortValue(rawValue.getValue());
+                        break;
+                    case NBTDefinitions.TAG_INT:
+                        this.writeIntegerValue(rawValue.getValue());
+                        break;
+                    case NBTDefinitions.TAG_LONG:
+                        this.writeLongValue(rawValue.getValue());
+                        break;
+                    case NBTDefinitions.TAG_FLOAT:
+                        this.writeFloatValue(rawValue.getValue());
+                        break;
+                    case NBTDefinitions.TAG_DOUBLE:
+                        this.writeDoubleValue(rawValue.getValue());
+                        break;
+                    case NBTDefinitions.TAG_BYTE_ARRAY:
+                        this.writeDoubleValue(rawValue);
+                        break;
+                    case NBTDefinitions.TAG_STRING:
+                        this.writeStringValue(rawValue.getValue());
+                        break;
+                    case NBTDefinitions.TAG_LIST:
+                        this.writeListValue(rawValue);
+                        break;
+                    case NBTDefinitions.TAG_COMPOUND:
+                        this.writeCompoundValue(rawValue);
+                        break;
+                    case NBTDefinitions.TAG_INT_ARRAY:
+                        this.writeIntegerArrayValue(rawValue);
+                        break;
+                }
+            }
+        } else {
+            this.writeByteValue(NBTDefinitions.TAG_BYTE);
+            this.writeIntegerValue(0);
+        }
+    }
+
+    private writeCompoundValue(compound: NBTTagCompound): void {
+        for (let [key, value] of compound.entries()) {
+            let nbtType = this.getNBTTypeFromValue(value);
+            this.writeTagHeader(nbtType, key);
+            switch (nbtType) {
+                case NBTDefinitions.TAG_BYTE:
+                    this.writeByteValue(value.getValue());
+                    break;
+                case NBTDefinitions.TAG_SHORT:
+                    this.writeShortValue(value.getValue());
+                    break;
+                case NBTDefinitions.TAG_INT:
+                    this.writeIntegerValue(value.getValue());
+                    break;
+                case NBTDefinitions.TAG_LONG:
+                    this.writeLongValue(value.getValue());
+                    break;
+                case NBTDefinitions.TAG_FLOAT:
+                    this.writeFloatValue(value.getValue());
+                    break;
+                case NBTDefinitions.TAG_DOUBLE:
+                    this.writeDoubleValue(value.getValue());
+                    break;
+                case NBTDefinitions.TAG_BYTE_ARRAY:
+                    this.writeByteArrayValue(value);
+                    break;
+                case NBTDefinitions.TAG_STRING:
+                    this.writeStringValue(value.getValue());
+                    break;
+                case NBTDefinitions.TAG_LIST:
+                    this.writeListValue(value);
+                    break;
+                case NBTDefinitions.TAG_COMPOUND:
+                    this.writeCompoundValue(value);
+                    break;
+                case NBTDefinitions.TAG_INT_ARRAY:
+                    this.writeIntegerValue(value);
+                    break;
+            }
+        }
+
+        this.writeByteValue(NBTDefinitions.TAG_END);
+    }
+
+    private getNBTTypeFromValue(value: any): NBTDefinitions {
+        if (value instanceof ByteVal) {
+            return NBTDefinitions.TAG_BYTE;
+        } else if (value instanceof ShortVal) {
+            return NBTDefinitions.TAG_SHORT;
+        } else if (value instanceof NumberVal) {
+            return NBTDefinitions.TAG_INT;
+        } else if (value instanceof LongVal) {
+            return NBTDefinitions.TAG_LONG;
+        } else if (value instanceof FloatVal) {
+            return NBTDefinitions.TAG_FLOAT;
+        } else if (value instanceof DoubleVal) {
+            return NBTDefinitions.TAG_DOUBLE;
+        } else if (value instanceof Buffer) {
+            return NBTDefinitions.TAG_BYTE_ARRAY;
+        } else if (value instanceof StringVal) {
+            return NBTDefinitions.TAG_STRING;
+        } else if (value instanceof Set) {
+            return NBTDefinitions.TAG_LIST;
+        } else if (value instanceof NBTTagCompound) {
+            return NBTDefinitions.TAG_COMPOUND;
+        } else if (Array.isArray(value)) {
+            return NBTDefinitions.TAG_INT_ARRAY;
+        } else {
+            throw new Error(`Invalid NBT Data: Cannot deduce NBT type of class ${value.constructor.name} (${value})`);
+        }
     }
 
     // Our buffer auto increments its capacity
