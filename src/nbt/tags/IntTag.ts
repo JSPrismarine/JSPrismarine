2b6c4ac7347298e0b509f50ca46b6989011b491e
HerryYT <enricoangelon.ea@gmail.com>
2020-11-14 07:58:23 +0100

    Initial nbt commit

--- src/nbt/tags/IntTag.ts
+++
@@ -1,6 +0,0 @@
-import Tag from './internal/Tag';
-import TagType from './internal/TagType';
-
-export default class IntTag extends Tag {
-    type = TagType.Int;
-}
--- src/nbt/tags/StringTag.ts
+++
@@ -1,6 +0,0 @@
-import Tag from './internal/Tag';
-import TagType from './internal/TagType';
-
-export default class StringTag extends Tag {
-    type = TagType.String;
-}
