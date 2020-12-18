db65243c7a7a567bf00dcf0f1cd701e97c2046c6
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-12-18 16:57:19 +0100

    :white_check_mark: Add some PluginManager tests.

---
+++ src/plugin/PluginManager.test.ts
@@ -0,0 +1,111 @@
+import mock from 'mock-fs';
+import path from 'path';
+import Prismarine from '../Prismarine';
+import LoggerBuilder from '../utils/Logger';
+import PluginManager from './PluginManager';
+
+jest.mock('winston', () => ({
+    format: {
+        colorize: jest.fn(),
+        combine: jest.fn(),
+        label: jest.fn(),
+        timestamp: jest.fn(),
+        simple: jest.fn(),
+        printf: jest.fn()
+    },
+    createLogger: jest.fn().mockReturnValue({
+        silly: jest.fn(),
+        debug: jest.fn(),
+        log: jest.fn(),
+        info: jest.fn()
+    }),
+    transports: {
+        Console: jest.fn(),
+        File: jest.fn()
+    }
+}));
+
+describe('plugin', () => {
+    describe('PluginManager', () => {
+        let server;
+        beforeAll(() => {
+            const logger = new LoggerBuilder();
+
+            server = server = new Prismarine({
+                logger,
+                config: new (class DebugConfig {
+                    public getPort() {
+                        return 19199;
+                    }
+                    public getServerIp() {
+                        return '0.0.0.0';
+                    }
+                    public getLevelName() {
+                        return '';
+                    }
+                    public getWorlds() {
+                        return {};
+                    }
+                    public getMaxPlayers() {
+                        return 1;
+                    }
+                    public getGamemode() {
+                        return 1;
+                    }
+                    public getMotd() {
+                        return 'CI';
+                    }
+                    public getViewDistance() {
+                        return 4;
+                    }
+                    public getOnlineMode() {
+                        return false;
+                    }
+                    public getEnableEval() {
+                        return false;
+                    }
+                    public getTelemetry() {
+                        return {
+                            enabled: false,
+                            urls: []
+                        };
+                    }
+                    public getPacketCompressionLevel() {
+                        return 7;
+                    }
+                })() as any
+            });
+        });
+        afterEach(mock.restore);
+
+        it('onEnable() should succeed with 0 plugins', async (done) => {
+            const pl = new PluginManager(server);
+            await pl.onEnable();
+            expect(pl.getPlugins().length).toEqual(0);
+
+            done();
+        });
+
+        it('onEnable() should succeed with valid plugin', async (done) => {
+            const pl = new PluginManager(server);
+
+            // Mock file-system
+            mock({
+                plugins: mock.load(
+                    path.resolve(process.cwd(), '.test/plugins')
+                ),
+                'src/plugin/api/versions': mock.load(
+                    path.resolve(process.cwd(), 'src/plugin/api/versions')
+                ),
+                node_modules: mock.load(
+                    path.resolve(__dirname, '../../node_modules')
+                )
+            });
+
+            await pl.onEnable();
+            expect(pl.getPlugins().length).toEqual(1);
+            expect(pl.getPlugins()[0].displayName).toBe('ci');
+            done();
+        });
+    });
+});
