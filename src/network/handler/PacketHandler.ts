cb76546b3d97a585a649f8cee52d6693ce4dae1e
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-01-04 16:09:40 +0100

    :zap: Throw error if block is missing.

--- src/network/handler/PacketHandler.ts
+++ src/network/handler/PacketHandler.ts
@@ -7,7 +7,7 @@ export default interface PacketHandler<T extends DataPacket> {
      * Handle a data packet.
      * @param packet Instance of packet we need to handle
      * @param server The server instance
-     * @param connection The playeer connection
+     * @param connection The player connection
      */
     handle(packet: T, server: Server, player: Player): Promise<void> | void;
 }
