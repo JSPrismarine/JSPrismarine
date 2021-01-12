8816cb774fb7a7d70fb06878d43ce1d8a467c966
Jakobg1215 <howtoblockcitywars@gmail.com>
2021-01-12 08:12:28 -0600

    Change some files for fix

--- src/block/blocks/LightBlueConcretePowder.ts
+++ src/block/blocks/LightBlueConcretePowder.ts
@@ -2,6 +2,9 @@ import ConcretePowder, { ConcretePowderColor } from './WhiteConcretePowder';
 
 export default class lightBlueConcrete extends ConcretePowder {
     constructor() {
-        super('minecraft:light_blue_concrete_powder', ConcretePowderColor.lightBlue);
+        super(
+            'minecraft:light_blue_concrete_powder',
+            ConcretePowderColor.lightBlue
+        );
     }
 }
