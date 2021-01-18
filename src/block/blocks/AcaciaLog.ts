715fb12bfdf60d3d63fb324bcd4b54dc668cac21
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-01-18 17:32:23 +0100

    :white_check_mark: Resolve #342.

--- src/block/blocks/AcaciaLog.ts
+++ src/block/blocks/AcaciaLog.ts
@@ -1,6 +1,6 @@
 import Log, { LogType } from './OakLog';
 export default class AcaciaLog extends Log {
-    constructor() {
+    public constructor() {
         super('minecraft:acacia_log', LogType.Acacia);
     }
 }
