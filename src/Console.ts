fcda0aea2da995fc384fa15a8c3a36512ada6a76
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-01-18 23:32:25 +0100

    :zap: Give console a runtimeId of -1.

--- src/Console.ts
+++ src/Console.ts
@@ -7,6 +7,7 @@ import readline from 'readline';
 export default class Console implements CommandExecuter {
     private readonly server: Server;
     private cli: readline.Interface;
+    public runtimeId = -1;
 
     public constructor(server: Server) {
         this.server = server;
