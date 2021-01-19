a47594f906362082bb478ffa62fe4afe2e51b6a2
bavfalcon9 <olybear9@gmail.com>
2021-01-19 01:37:25 -0600

    chore: Add more argument parsing stuff

--- src/command/Command.ts
+++ src/command/Command.ts
@@ -4,7 +4,7 @@ import type { CommandDispatcher } from '@jsprismarine/brigadier';
 import CommandExecuter from './CommandExecuter';
 import type Player from '../player/Player';
 
-type Promiseable<T> = Promise<T> | T;
+export type Promiseable<T> = Promise<T> | T;
 
 interface CommandProps {
     id: string;
