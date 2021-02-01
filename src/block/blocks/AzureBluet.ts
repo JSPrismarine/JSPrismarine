937f240668eb8ec97d852740e1c479563b2551c2
BoredSathvik <68980580+BoredSathvik@users.noreply.github.com>
2021-02-01 21:32:14 +0530

    feat(blocks): Added Flowers (#368).
    * Added colored beds
    
    * Added Flowers
    
    * remove unnecessary deloratations
    
    * [CodeFactor] Apply fixes to commit c32f33c
    
    * Update Poppy.ts
    
    * chore: Consistency across project with enums
    
    * chore: Remove unnecessary space
    
    Co-authored-by: srmdev <68980580+srmdev@users.noreply.github.com>
    Co-authored-by: Bavfalcon9 <olybear9@gmail.com>
    Co-authored-by: codefactor-io <support@codefactor.io>
    Co-authored-by: Filiph Sandström <filiph.sandstrom@filfatstudios.com>

---
+++ src/block/blocks/AzureBluet.ts
@@ -0,0 +1,7 @@
+import Poppy, { FlowerType } from './Poppy';
+
+export default class AzureBluet extends Poppy {
+    public constructor() {
+        super('minecraft:azure_bluet', FlowerType.AzureBluet);
+    }
+}
---
+++ src/block/blocks/OxeyeDaisy.ts
@@ -0,0 +1,7 @@
+import Poppy, { FlowerType } from './Poppy';
+
+export default class OxeyeDaisy extends Poppy {
+    public constructor() {
+        super('minecraft:oxeye_daisy', FlowerType.OxeyeDaisy);
+    }
+}
