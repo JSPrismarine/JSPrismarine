a0b90e928cb566f2d57426250e5e1fcb73ef27ba
Jakob <74737820+Jakobg1215@users.noreply.github.com>
2021-02-11 22:21:11 -0600

    feat(events): Add Command Register Event (#385).

---
+++ src/events/command/CommandRegisterEvents.ts
@@ -0,0 +1,15 @@
+import type Command from '../../command/Command';
+import Event from '../Event';
+
+export default class CommandRegisterEvent extends Event {
+    private readonly Command;
+
+    public constructor(command: Command) {
+        super();
+        this.Command = command;
+    }
+
+    public getCommand(): Command {
+        return this.Command;
+    }
+}
