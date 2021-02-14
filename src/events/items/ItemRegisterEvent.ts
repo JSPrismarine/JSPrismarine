7d0a9ca88d8d76909148668662bc4f76c9b1c504
Jakob <74737820+Jakobg1215@users.noreply.github.com>
2021-02-14 07:19:19 -0600

    fix(command): Fix registered count (#389).
    * Add Item Register Event
    * Fix Debug registered count

---
+++ src/events/items/ItemRegisterEvent.ts
@@ -0,0 +1,15 @@
+import Event from '../Event';
+import type Item from '../../item/Item';
+
+export default class ItemRegisterEvent extends Event {
+    private readonly item;
+
+    public constructor(item: Item) {
+        super();
+        this.item = item;
+    }
+
+    public getItem(): Item {
+        return this.item;
+    }
+}
