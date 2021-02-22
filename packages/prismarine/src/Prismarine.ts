6e760e78e326a3496476d33ce6067552e666088d
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-02-22 02:20:58 +0000

    fix(build): Try to fix CI build.

--- packages/prismarine/src/Prismarine.ts
+++ packages/prismarine/src/Prismarine.ts
@@ -4,6 +4,5 @@ import * as Protocol from './network/Protocol';
 import Config from './config/Config';
 import Logger from './utils/Logger';
 import Server from './Server';
-import Updater from './updater/Updater';
 
-export { Server, Logger, Config, Updater, Events, Protocol };
+export { Server, Logger, Config, Events, Protocol };
