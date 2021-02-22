475b367a7865eab78ce282c167eb3276bab58050
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-02-22 14:09:21 +0100

    feat(server-fill): start work on server-fill package.

--- packages/server-fill/src/ServerFill.ts
+++ packages/server-fill/src/ServerFill.ts
@@ -1,1 +1,17 @@
 import Client from '@jsprismarine/client';
+
+const ip = process.argv[2].split(':')[0];
+const port = process.argv[2].split(':')[1];
+
+const clients = new Array(5);
+for (let i = 0; i < clients.length; i++) {
+    clients[i] = new Client();
+}
+
+Promise.all(
+    clients.map(async (c) => {
+        await c.connect(ip, port);
+    })
+).then(() => {
+    console.log('connected');
+});
