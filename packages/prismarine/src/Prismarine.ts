c3cc4547826ceae4e3b4c681d1eb99ea42fc3f5a
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-02-22 02:36:25 +0000

    fix(build): HOTFIX.

--- packages/prismarine/src/Prismarine.ts
+++ packages/prismarine/src/Prismarine.ts
@@ -2,7 +2,8 @@ import * as Events from './events/Events';
 import * as Protocol from './network/Protocol';
 
 import Config from './config/Config';
+import ConfigBuilder from './config/ConfigBuilder';
 import Logger from './utils/Logger';
 import Server from './Server';
 
-export { Server, Logger, Config, Events, Protocol };
+export { Server, Logger, Config, ConfigBuilder, Events, Protocol };
