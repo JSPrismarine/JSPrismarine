6434a4d8fd2dbdbe2e5532f4ee32cead14a46d81
Jakob Gonyer <howtoblockcitywars@gmail.com>
2021-03-02 20:03:25 -0600

    add custom flat world generation

--- packages/prismarine/src/world/generators/Flat.ts
+++ packages/prismarine/src/world/generators/Flat.ts
@@ -1,22 +1,34 @@
 import BaseGenerator from '../BaseGenerator';
+import type Block from '../../block/Block';
 import Chunk from '../chunk/Chunk';
+import Config from '../../config/Config';
 
 export default class Flat extends BaseGenerator {
     public async generateChunk(cx: number, cz: number): Promise<Chunk> {
         return new Promise((resolve) => {
             const chunk = new Chunk(cx, cz);
 
-            const bedrock = this.getBlockManager().getBlock('minecraft:bedrock');
-            const dirt = this.getBlockManager().getBlock('minecraft:dirt');
-            const grass = this.getBlockManager().getBlock('minecraft:grass');
+            const layers = new Config('flat').getWorlds().world.layers;
+            const blocks: Block[] = [];
+            layers.forEach((layer: any) => {
+                let block: Block;
+                try {
+                    if (!layer.block.startsWith('minecraft:')) {
+                        block = this.getBlockManager().getBlock(`minecraft:${layer.block}`);
+                    } else block = this.getBlockManager().getBlock(layer.block);
+                } catch {
+                    block = this.getBlockManager().getBlock('minecraft:air');
+                }
+                for (let c = 1; c <= layer.size; c++) {
+                    blocks.push(block);
+                }
+            });
 
             for (let x = 0; x < 16; x++) {
                 for (let z = 0; z < 16; z++) {
-                    chunk.setBlock(x, 0, z, bedrock);
-                    chunk.setBlock(x, 1, z, dirt);
-                    chunk.setBlock(x, 2, z, dirt);
-                    chunk.setBlock(x, 3, z, dirt);
-                    chunk.setBlock(x, 4, z, grass);
+                    blocks.forEach((block, place) => {
+                        chunk.setBlock(x, place, z, block);
+                    });
                 }
             }
 
