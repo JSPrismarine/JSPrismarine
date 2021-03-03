f9af6df70814a7ab31d0db009c459cc99a76e695
Jakob <74737820+Jakobg1215@users.noreply.github.com>
2021-03-02 21:35:08 -0600

    Fix for negative numbers.

--- packages/prismarine/src/command/vanilla/TpCommand.ts
+++ packages/prismarine/src/command/vanilla/TpCommand.ts
@@ -28,8 +28,8 @@ export default class TpCommand extends Command {
 
                         const position = context.getArgument('position') as Vector3;
 
-                        if (Number.isInteger(position.getX())) position.setX(position.getX() + 0.5);
-                        if (Number.isInteger(position.getZ())) position.setZ(position.getZ() + 0.5);
+                        if (!Number.isInteger(position.getX())) position.getX() > 0 ? position.setX(position.getX() - 0.5) : position.setX(position.getX() + 0.5);
+                        if (!Number.isInteger(position.getZ())) position.getZ() > 0 ? position.setZ(position.getZ() - 0.5) : position.setZ(position.getZ() + 0.5);
 
                         await source.setPosition(position, MovementType.Teleport);
                         return `Teleported ${source.getFormattedUsername()} to ${position.getX()} ${position.getY()} ${position.getZ()}`;
@@ -42,6 +42,9 @@ export default class TpCommand extends Command {
                                 const targets = context.getArgument('player') as Player[];
 
                                 const position = context.getArgument('position') as Vector3;
+                                
+                                if (!Number.isInteger(position.getX())) position.getX() > 0 ? position.setX(position.getX() - 0.5) : position.setX(position.getX() + 0.5);
+                                if (!Number.isInteger(position.getZ())) position.getZ() > 0 ? position.setZ(position.getZ() - 0.5) : position.setZ(position.getZ() + 0.5);
 
                                 if (!targets?.length) throw new Error(`Cannot find specified player(s) & entit(y/ies)`);
 
