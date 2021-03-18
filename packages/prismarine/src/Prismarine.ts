04a6aebb89b7916466da0ba393de16195aa8f709
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-03-18 01:00:37 +0000

    feat(network): Start exporting types too.

--- packages/prismarine/src/Prismarine.ts
+++ packages/prismarine/src/Prismarine.ts
@@ -4,6 +4,8 @@ import * as Protocol from './network/Protocol';
 import Config from './config/Config';
 import ConfigBuilder from './config/ConfigBuilder';
 import Logger from './utils/Logger';
+import Player from './player/Player';
+import PlayerConnection from './player/PlayerConnection';
 import Server from './Server';
 
-export { Server, Logger, Config, ConfigBuilder, Events, Protocol };
+export { Server, Logger, Config, ConfigBuilder, Events, Protocol, Player, PlayerConnection };
---
+++ packages/prismarine/src/network/Packets.ts
@@ -0,0 +1,88 @@
+import ActorFallPacket from './packet/ActorFallPacket';
+import AddActorPacket from './packet/AddActorPacket';
+import AddPlayerPacket from './packet/AddPlayerPacket';
+import AdventureSettingsPacket from './packet/AdventureSettingsPacket';
+import AnimatePacket from './packet/AnimatePacket';
+import AvailableActorIdentifiersPacket from './packet/AvailableActorIdentifiersPacket';
+import AvailableCommandsPacket from './packet/AvailableCommandsPacket';
+import BatchPacket from './packet/BatchPacket';
+import BiomeDefinitionListPacket from './packet/BiomeDefinitionListPacket';
+import ChangeDimensionPacket from './packet/ChangeDimensionPacket';
+import ChunkRadiusUpdatedPacket from './packet/ChunkRadiusUpdatedPacket';
+import ClientCacheStatusPacket from './packet/ClientCacheStatusPacket';
+import CommandBlockUpdatePacket from './packet/CommandBlockUpdatePacket';
+import CommandRequestPacket from './packet/CommandRequestPacket';
+import ContainerClosePacket from './packet/ContainerClosePacket';
+import ContainerOpenPacket from './packet/ContainerOpenPacket';
+import CreativeContentPacket from './packet/CreativeContentPacket';
+import DataPacket from './packet/DataPacket';
+import DisconnectPacket from './packet/DisconnectPacket';
+import EmoteListPacket from './packet/EmoteListPacket';
+import InteractPacket from './packet/InteractPacket';
+import InventoryContentPacket from './packet/InventoryContentPacket';
+import InventoryTransactionPacket from './packet/InventoryTransactionPacket';
+import ItemComponentPacket from './packet/ItemComponentPacket';
+import ItemStackRequestPacket from './packet/ItemStackRequestPacket';
+import ItemStackResponsePacket from './packet/ItemStackResponsePacket';
+import LevelChunkPacket from './packet/LevelChunkPacket';
+import LevelSoundEventPacket from './packet/LevelSoundEventPacket';
+import LoginPacket from './packet/LoginPacket';
+import MobEquipmentPacket from './packet/MobEquipmentPacket';
+import ModalFormRequestPacket from './packet/ModalFormRequestPacket';
+import ModalFormResponsePacket from './packet/ModalFormResponsePacket';
+import MoveActorAbsolutePacket from './packet/MoveActorAbsolutePacket';
+import MovePlayerPacket from './packet/MovePlayerPacket';
+import NetworkChunkPublisherUpdatePacket from './packet/NetworkChunkPublisherUpdatePacket';
+import OnScreenTextureAnimationPacket from './packet/OnScreenTextureAnimationPacket';
+import PacketViolationWarningPacket from './packet/PacketViolationWarningPacket';
+import PlayerActionPacket from './packet/PlayerActionPacket';
+import PlayerListPacket from './packet/PlayerListPacket';
+import StartGamePacket from './packet/StartGamePacket';
+import TextPacket from './packet/TextPacket';
+import TickSyncPacket from './packet/TickSyncPacket';
+
+// TODO: export all packets
+export {
+    ActorFallPacket,
+    AddActorPacket,
+    AddPlayerPacket,
+    AdventureSettingsPacket,
+    AnimatePacket,
+    AvailableActorIdentifiersPacket,
+    AvailableCommandsPacket,
+    BatchPacket, // Special packet
+    BiomeDefinitionListPacket,
+    ChangeDimensionPacket,
+    ChunkRadiusUpdatedPacket,
+    ClientCacheStatusPacket,
+    CommandBlockUpdatePacket,
+    CommandRequestPacket,
+    ContainerClosePacket,
+    ContainerOpenPacket,
+    CreativeContentPacket,
+    DataPacket, // Special packet
+    DisconnectPacket,
+    EmoteListPacket,
+    InteractPacket,
+    InventoryContentPacket,
+    InventoryTransactionPacket,
+    ItemComponentPacket,
+    ItemStackRequestPacket,
+    ItemStackResponsePacket,
+    LevelChunkPacket,
+    LevelSoundEventPacket,
+    LoginPacket,
+    MobEquipmentPacket,
+    ModalFormRequestPacket,
+    ModalFormResponsePacket,
+    MovePlayerPacket,
+    MoveActorAbsolutePacket,
+    NetworkChunkPublisherUpdatePacket,
+    OnScreenTextureAnimationPacket,
+    PacketViolationWarningPacket,
+    PlayerActionPacket,
+    PlayerListPacket,
+    StartGamePacket,
+    TextPacket,
+    TickSyncPacket
+};
