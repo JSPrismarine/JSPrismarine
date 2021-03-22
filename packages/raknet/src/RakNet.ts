a39b0b2248bb08f0af27354992314045bd1fa3ef
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-03-22 03:15:04 +0100

    fix(package): Resolve #428. (#438).
    * fix(package): Resolve #428.
    
    * fix(build): Fix build.
    
    * fix(build): Fix build.

--- packages/raknet/src/RakNet.ts
+++ packages/raknet/src/RakNet.ts
@@ -1,8 +1,10 @@
 import * as Protocol from './protocol/Protocol';
+
 import Connection, { Priority as ConnectionPriority } from './Connection';
+
+import InetAddress from './utils/InetAddress';
 import Listener from './Listener';
 import RakNetListener from './RakNetListener';
-import InetAddress from './utils/InetAddress';
 import ServerName from './utils/ServerName';
 
 export type { RakNetListener };
