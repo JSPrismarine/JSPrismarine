5e6345e81fca830807bf4c159ccb9de04a742ffb
Jakob Gonyer <howtoblockcitywars@gmail.com>
2021-03-21 19:03:15 -0500

    Add more exports

--- packages/prismarine/src/Prismarine.ts
+++ packages/prismarine/src/Prismarine.ts
@@ -3,11 +3,14 @@ import * as Events from './events/Events';
 import * as Math from './math/Math';
 import * as Protocol from './network/Protocol';
 
+import Chat from './chat/Chat';
 import Config from './config/Config';
 import ConfigBuilder from './config/ConfigBuilder';
+import ContainerEntry from './inventory/ContainerEntry';
 import Logger from './utils/Logger';
 import Player from './player/Player';
 import PlayerConnection from './player/PlayerConnection';
+import PluginApi from './plugin/api/versions/1.0/PluginApi';
 import Server from './Server';
 
-export { Server, Logger, Config, ConfigBuilder, Math, Entities, Events, Protocol, Player, PlayerConnection };
+export { Server, Logger, Chat, Config, ConfigBuilder, ContainerEntry, Math, Entities, Events, Protocol, Player, PlayerConnection, PluginApi };
