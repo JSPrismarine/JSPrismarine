d4ef1d209f4f964f5427e964abcae2cdc98db04f
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-04-12 00:08:07 +0200

    docs(utils): Document utils.

--- packages/prismarine/src/utils/Timer.ts
+++ packages/prismarine/src/utils/Timer.ts
@@ -1,23 +1,50 @@
+/**
+ * Timer/Performance measurement utility.
+ *
+ * @public
+ */
 export default class Timer {
-    private startTime: any;
-    private endTime?: any;
+    /**
+     * The start time.
+     */
+    private startTime: [number, number];
+
+    /**
+     * The end time.
+     */
+    private endTime!: [number, number];
 
     public constructor() {
         this.startTime = process.hrtime();
     }
 
+    /**
+     * Reset the timer.
+     */
     public reset() {
         this.startTime = process.hrtime();
-        this.endTime = null;
+        this.endTime = undefined!;
     }
 
+    /**
+     * Stop the timer.
+     *
+     * @returns The total duration in ms as a `float`
+     */
     public stop(): number {
         this.endTime = process.hrtime(this.startTime);
 
         return this.getResult();
     }
 
+    /**
+     * Get the total duration.
+     *
+     * @returns The total duration in ms as a `float`
+     */
     public getResult(): number {
+        if (!this.endTime) throw new Error(`You need to stop the timer before getting the result.`);
+
         return Number.parseFloat(((this.endTime[0] * 1e9 + this.endTime[1]) / 1_000_000).toFixed(3));
     }
 }
