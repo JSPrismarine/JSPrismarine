45d8a81f753dc2200b2d03418f1efff8084de244
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-04-21 14:41:28 +0200

    chore(package): Add npm-run-all.

---
+++ packages/prismarine/src/Managers.ts
@@ -0,0 +1,33 @@
+import BanManager from './ban/BanManager';
+import BlockManager from './block/BlockManager';
+import ChatManager from './chat/ChatManager';
+import CommandManager from './command/CommandManager';
+import { EventManager } from './events/EventManager';
+import FormManager from './form/FormManager';
+import ItemManager from './item/ItemManager';
+import PermissionManager from './permission/PermissionManager';
+import PlayerManager from './player/PlayerManager';
+import PluginManager from './plugin/PluginManager';
+import QueryManager from './query/QueryManager';
+import TelemeteryManager from './telemetry/TelemeteryManager';
+import WindowManager from './inventory/WindowManager';
+import WorkerManager from './worker/WorkerManager';
+import WorldManager from './world/WorldManager';
+
+export {
+    BanManager,
+    BlockManager,
+    ChatManager,
+    CommandManager,
+    EventManager,
+    FormManager,
+    ItemManager,
+    PermissionManager,
+    PlayerManager,
+    PluginManager,
+    QueryManager,
+    TelemeteryManager,
+    WindowManager,
+    WorkerManager,
+    WorldManager
+};
