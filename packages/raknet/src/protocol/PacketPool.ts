299cb6216ccdf6bbecbcae12870c62d19a853c10
Enrico Angelon <enricoangelon.ea@gmail.com>
2023-01-13 17:11:20 +0100

    refactor(raknet): use lookup table and packet pool
    * added a lookup table for packet handlers
    * fixed an issue where session.disconnect() in raknet was being called multiple times
    * use a packet pool to reduce allocations

---
+++ packages/raknet/src/protocol/PacketPool.ts
@@ -0,0 +1,35 @@
+import ACK from "./ACK.js";
+import FrameSet from "./FrameSet.js";
+import NACK from "./NACK.js";
+
+export default class PacketPool {
+    private readonly ackPool: ACK[] = [];
+    private readonly nackPool: NACK[] = [];
+    private readonly framesetPool: FrameSet[] = [];
+
+    public getAckInstance(): ACK {
+        return this.ackPool.pop() ?? new ACK();
+    }
+
+    public getNackInstance(): NACK {
+        return this.nackPool.pop() ?? new NACK();
+    }
+
+    public getFrameSetInstance(): FrameSet {
+        return this.framesetPool.pop() ?? new FrameSet();
+    }
+
+    public returnAck(ack: ACK): void {
+        this.ackPool.push(ack);
+    }
+
+    public returnNack(nack: NACK): void {
+        this.nackPool.push(nack);
+    }
+
+    public returnFrameSet(frameSet: FrameSet): void {
+        frameSet.frames = [];
+        frameSet.sequenceNumber = -1;
+        this.framesetPool.push(frameSet);
+    }
+}
