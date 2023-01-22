3a48794f6a96612668e44bb2242512f85a35e73d
Enrico Angelon <enricoangelon.ea@gmail.com>
2023-01-22 23:24:24 +0100

    fix!: proper inventory transaction encoding

--- packages/prismarine/src/network/Types.ts
+++ packages/prismarine/src/network/Types.ts
@@ -1,7 +1,6 @@
 import AnimateActionType from './type/AnimateActionType.js';
-import ChangeSlot from './type/ChangeSlot.js';
 import TextType from './type/TextType.js';
 
-export { AnimateActionType, ChangeSlot, TextType };
+export { AnimateActionType, TextType };
 
 // TODO: get rid of this and types in general.
