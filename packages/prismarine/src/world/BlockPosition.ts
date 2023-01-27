b350260b92a4d2c2d836a43285dfed291ef37c5e
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2023-01-27 20:33:43 +0200

    Fixed the z typo

--- packages/prismarine/src/world/BlockPosition.ts
+++ packages/prismarine/src/world/BlockPosition.ts
@@ -5,7 +5,7 @@ export default class BlockPosition extends Vector3 {
     public networkSerialize(stream: any): void {
         stream.writeVarInt(this.getX());
         stream.writeUnsignedVarInt(this.getY());
-        stream.writeVarInt(this.getX());
+        stream.writeVarInt(this.getZ());
     }
 
     public static networkDeserialize(stream: any): BlockPosition {
