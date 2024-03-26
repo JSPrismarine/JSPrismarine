720da6e13de2567d1ec7806b8d08c11109161dc7
codefactor-io <support@codefactor.io>
2024-03-26 17:00:32 +0000

    [CodeFactor] Apply fixes

--- packages/prismarine/src/network/handler/RequestNetworkSettingsHandler.ts
+++ packages/prismarine/src/network/handler/RequestNetworkSettingsHandler.ts
@@ -1,33 +1,40 @@
-import Identifiers from '../Identifiers';
-import type Server from '../../Server';
-import RequestNetworkSettingsPacket from '../packet/RequestNetworkSettingsPacket';
-import PreLoginPacketHandler from './PreLoginPacketHandler';
-import ClientConnection from '../ClientConnection';
-import NetworkSettingsPacket, { PacketCompressionAlgorithm, CompressionThreshold } from '../packet/NetworkSettingsPacket';
+import Identifiers from "../Identifiers";
+import type Server from "../../Server";
+import type RequestNetworkSettingsPacket from "../packet/RequestNetworkSettingsPacket";
+import type PreLoginPacketHandler from "./PreLoginPacketHandler";
+import type ClientConnection from "../ClientConnection";
+import NetworkSettingsPacket, {
+  PacketCompressionAlgorithm,
+  CompressionThreshold,
+} from "../packet/NetworkSettingsPacket";
 
-export default class RequestNetworkSettingsHandler implements PreLoginPacketHandler<RequestNetworkSettingsPacket> {
-    public static NetID = Identifiers.RequestNetworkSettingsPacket;
+export default class RequestNetworkSettingsHandler
+  implements PreLoginPacketHandler<RequestNetworkSettingsPacket> {
+  public static NetID = Identifiers.RequestNetworkSettingsPacket;
 
-    public async handle(
-        packet: RequestNetworkSettingsPacket,
-        _server: Server,
-        connection: ClientConnection
-    ): Promise<void> {
-        if (packet.protocolVersion !== Identifiers.Protocol) {
-            connection.disconnect(`Unsupported protocol version: ${packet.protocolVersion}`);
-            return;
-        }
-
-        const networkSettings = new NetworkSettingsPacket();
-        networkSettings.compressionThreshold = CompressionThreshold.COMPRESS_EVERYTHING;
-        networkSettings.compressionAlgorithm = PacketCompressionAlgorithm.ZLIB;
-        networkSettings.clientThrottlingEnabled = false;
-        networkSettings.clientThrottleThreshold = 0;
-        networkSettings.clientThrottleScalar = 0;
-
-        connection.hasCompression = true;
-
-        // Send as uncompressed, this will initialize compression
-        await connection.sendDataPacket(networkSettings, false);
+  public async handle(
+    packet: RequestNetworkSettingsPacket,
+    _server: Server,
+    connection: ClientConnection,
+  ): Promise<void> {
+    if (packet.protocolVersion !== Identifiers.Protocol) {
+      connection.disconnect(
+        `Unsupported protocol version: ${packet.protocolVersion}`,
+      );
+      return;
     }
+
+    const networkSettings = new NetworkSettingsPacket();
+    networkSettings.compressionThreshold =
+      CompressionThreshold.COMPRESS_EVERYTHING;
+    networkSettings.compressionAlgorithm = PacketCompressionAlgorithm.ZLIB;
+    networkSettings.clientThrottlingEnabled = false;
+    networkSettings.clientThrottleThreshold = 0;
+    networkSettings.clientThrottleScalar = 0;
+
+    connection.hasCompression = true;
+
+    // Send as uncompressed, this will initialize compression
+    await connection.sendDataPacket(networkSettings, false);
+  }
 }
