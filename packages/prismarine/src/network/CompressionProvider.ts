6bd9620f3ac92350606e75651b943562fc83a5c9
codefactor-io <support@codefactor.io>
2024-03-26 17:00:32 +0000

    [CodeFactor] Apply fixes

--- packages/prismarine/src/network/CompressionProvider.ts
+++ packages/prismarine/src/network/CompressionProvider.ts
@@ -1,36 +1,40 @@
-import { inflateRaw, inflateRawSync } from 'zlib';
-import { PacketCompressionAlgorithm } from './packet/NetworkSettingsPacket.js';
-import { promisify } from 'util';
+import { inflateRaw, inflateRawSync } from "zlib";
+import { PacketCompressionAlgorithm } from "./packet/NetworkSettingsPacket.js";
+import { promisify } from "util";
 
 const asyncInflate = promisify(inflateRaw);
 
 export default class CompressionProvider {
-    public static fromAlgorithm(algorithm: PacketCompressionAlgorithm): (buffer: Buffer) => Promise<Buffer> {
-        switch (algorithm) {
-            case PacketCompressionAlgorithm.ZLIB:
-                // return new ZlibCompressionProvider();
-                return asyncInflate;
-            case PacketCompressionAlgorithm.NONE:
-                return async (buffer: Buffer) => buffer;
-            case PacketCompressionAlgorithm.SNAPPY:
-                throw new Error('Snappy compression is not implemented');
-            default:
-                throw new Error(`Unsupported compression algorithm: ${algorithm}`);
-        }
+  public static fromAlgorithm(
+    algorithm: PacketCompressionAlgorithm,
+  ): (buffer: Buffer) => Promise<Buffer> {
+    switch (algorithm) {
+      case PacketCompressionAlgorithm.ZLIB:
+        // return new ZlibCompressionProvider();
+        return asyncInflate;
+      case PacketCompressionAlgorithm.NONE:
+        return async (buffer: Buffer) => buffer;
+      case PacketCompressionAlgorithm.SNAPPY:
+        throw new Error("Snappy compression is not implemented");
+      default:
+        throw new Error(`Unsupported compression algorithm: ${algorithm}`);
     }
+  }
 
-    // TODO: move to full async... and remove this method...
-    public static fromAlgorithmSync(algorithm: PacketCompressionAlgorithm): (buffer: Buffer) => Buffer {
-        switch (algorithm) {
-            case PacketCompressionAlgorithm.ZLIB:
-                // return new ZlibCompressionProvider();
-                return inflateRawSync;
-            case PacketCompressionAlgorithm.NONE:
-                return (buffer: Buffer) => buffer;
-            case PacketCompressionAlgorithm.SNAPPY:
-                throw new Error('Snappy compression is not implemented');
-            default:
-                throw new Error(`Unsupported compression algorithm: ${algorithm}`);
-        }
+  // TODO: move to full async... and remove this method...
+  public static fromAlgorithmSync(
+    algorithm: PacketCompressionAlgorithm,
+  ): (buffer: Buffer) => Buffer {
+    switch (algorithm) {
+      case PacketCompressionAlgorithm.ZLIB:
+        // return new ZlibCompressionProvider();
+        return inflateRawSync;
+      case PacketCompressionAlgorithm.NONE:
+        return (buffer: Buffer) => buffer;
+      case PacketCompressionAlgorithm.SNAPPY:
+        throw new Error("Snappy compression is not implemented");
+      default:
+        throw new Error(`Unsupported compression algorithm: ${algorithm}`);
     }
+  }
 }
--- packages/prismarine/src/network/handler/ResourcePackResponseHandler.ts
+++ packages/prismarine/src/network/handler/ResourcePackResponseHandler.ts
@@ -1,180 +1,197 @@
-import Chat, { ChatType } from '../../chat/Chat';
-import RespawnPacket, { RespawnState } from '../packet/RespawnPacket';
-import SetSpawnPositionPacket, { SpawnType } from '../packet/SetSpawnPositionPacket';
+import Chat, { ChatType } from "../../chat/Chat";
+import RespawnPacket, { RespawnState } from "../packet/RespawnPacket";
+import SetSpawnPositionPacket, {
+  SpawnType,
+} from "../packet/SetSpawnPositionPacket";
 
-import AvailableActorIdentifiersPacket from '../packet/AvailableActorIdentifiersPacket';
-import BiomeDefinitionListPacket from '../packet/BiomeDefinitionListPacket';
-import ChatEvent from '../../events/chat/ChatEvent';
-import Gamemode from '../../world/Gamemode';
-import Identifiers from '../Identifiers';
-import ItemComponentPacket from '../packet/ItemComponentPacket';
-import type PacketHandler from './PacketHandler';
-import PlayStatusType from '../type/PlayStatusType';
-import type { PlayerSession } from '@';
-import PlayerSpawnEvent from '../../events/player/PlayerSpawnEvent';
-import type ResourcePackResponsePacket from '../packet/ResourcePackResponsePacket';
-import ResourcePackStackPacket from '../packet/ResourcePackStackPacket';
-import ResourcePackStatusType from '../type/ResourcePackStatusType';
-import type Server from '../../Server';
-import StartGamePacket from '../packet/StartGamePacket';
-import Vector3 from '../../math/Vector3';
+import AvailableActorIdentifiersPacket from "../packet/AvailableActorIdentifiersPacket";
+import BiomeDefinitionListPacket from "../packet/BiomeDefinitionListPacket";
+import ChatEvent from "../../events/chat/ChatEvent";
+import Gamemode from "../../world/Gamemode";
+import Identifiers from "../Identifiers";
+import ItemComponentPacket from "../packet/ItemComponentPacket";
+import type PacketHandler from "./PacketHandler";
+import PlayStatusType from "../type/PlayStatusType";
+import type { PlayerSession } from "@";
+import PlayerSpawnEvent from "../../events/player/PlayerSpawnEvent";
+import type ResourcePackResponsePacket from "../packet/ResourcePackResponsePacket";
+import ResourcePackStackPacket from "../packet/ResourcePackStackPacket";
+import ResourcePackStatusType from "../type/ResourcePackStatusType";
+import type Server from "../../Server";
+import StartGamePacket from "../packet/StartGamePacket";
+import Vector3 from "../../math/Vector3";
 
-export default class ResourcePackResponseHandler implements PacketHandler<ResourcePackResponsePacket> {
-    public static NetID = Identifiers.ResourcePackResponsePacket;
+export default class ResourcePackResponseHandler
+  implements PacketHandler<ResourcePackResponsePacket> {
+  public static NetID = Identifiers.ResourcePackResponsePacket;
 
-    public async handle(packet: ResourcePackResponsePacket, server: Server, session: PlayerSession): Promise<void> {
-        if (packet.status === ResourcePackStatusType.HaveAllPacks) {
-            const resourcePackStack = new ResourcePackStackPacket();
-            resourcePackStack.texturePackRequired = false;
-            resourcePackStack.experimentsAlreadyEnabled = false;
-            await session.getConnection().sendDataPacket(resourcePackStack);
-        } else if (packet.status === ResourcePackStatusType.Completed) {
-            const player = session.getPlayer();
-            // Emit playerSpawn event
-            const spawnEvent = new PlayerSpawnEvent(player);
-            server.getEventManager().post(['playerSpawn', spawnEvent]);
-            if (spawnEvent.isCancelled()) return;
+  public async handle(
+    packet: ResourcePackResponsePacket,
+    server: Server,
+    session: PlayerSession,
+  ): Promise<void> {
+    if (packet.status === ResourcePackStatusType.HaveAllPacks) {
+      const resourcePackStack = new ResourcePackStackPacket();
+      resourcePackStack.texturePackRequired = false;
+      resourcePackStack.experimentsAlreadyEnabled = false;
+      await session.getConnection().sendDataPacket(resourcePackStack);
+    } else if (packet.status === ResourcePackStatusType.Completed) {
+      const player = session.getPlayer();
+      // Emit playerSpawn event
+      const spawnEvent = new PlayerSpawnEvent(player);
+      server.getEventManager().post(["playerSpawn", spawnEvent]);
+      if (spawnEvent.isCancelled()) return;
 
-            // TODO: send inventory slots
+      // TODO: send inventory slots
 
-            await session.addToPlayerList();
+      await session.addToPlayerList();
 
-            const world = player.getWorld();
+      const world = player.getWorld();
 
-            await session.sendTime(world.getTicks());
+      await session.sendTime(world.getTicks());
 
-            const startGame = new StartGamePacket();
-            startGame.entityId = player.getRuntimeId();
-            startGame.runtimeEntityId = player.getRuntimeId();
-            startGame.gamemode = 1;
-            startGame.defaultGamemode = Gamemode.getGamemodeId(server.getConfig().getGamemode());
+      const startGame = new StartGamePacket();
+      startGame.entityId = player.getRuntimeId();
+      startGame.runtimeEntityId = player.getRuntimeId();
+      startGame.gamemode = 1;
+      startGame.defaultGamemode = Gamemode.getGamemodeId(
+        server.getConfig().getGamemode(),
+      );
 
-            const worldSpawnPos = await world.getSpawnPosition();
-            startGame.worldSpawnPos = worldSpawnPos;
+      const worldSpawnPos = await world.getSpawnPosition();
+      startGame.worldSpawnPos = worldSpawnPos;
 
-            startGame.playerPos = new Vector3(player.getX(), player.getY(), player.getZ());
-            startGame.pitch = player.pitch;
-            startGame.yaw = player.yaw;
+      startGame.playerPos = new Vector3(
+        player.getX(),
+        player.getY(),
+        player.getZ(),
+      );
+      startGame.pitch = player.pitch;
+      startGame.yaw = player.yaw;
 
-            startGame.levelId = world.getUniqueId();
-            startGame.worldName = world.getName();
-            startGame.seed = world.getSeed();
-            startGame.gamerules = world.getGameruleManager();
-            await session.getConnection().sendDataPacket(startGame);
+      startGame.levelId = world.getUniqueId();
+      startGame.worldName = world.getName();
+      startGame.seed = world.getSeed();
+      startGame.gamerules = world.getGameruleManager();
+      await session.getConnection().sendDataPacket(startGame);
 
-            const itemComponent = new ItemComponentPacket();
-            await session.getConnection().sendDataPacket(itemComponent);
+      const itemComponent = new ItemComponentPacket();
+      await session.getConnection().sendDataPacket(itemComponent);
 
-            const setSpawPos = new SetSpawnPositionPacket();
-            setSpawPos.dimension = 3; // TODO: enum
-            setSpawPos.position = new Vector3(-2147483648, -2147483648, -2147483648);
-            setSpawPos.blockPosition = setSpawPos.position;
-            setSpawPos.type = SpawnType.PLAYER_SPAWN;
-            await session.getConnection().sendDataPacket(setSpawPos);
+      const setSpawPos = new SetSpawnPositionPacket();
+      setSpawPos.dimension = 3; // TODO: enum
+      setSpawPos.position = new Vector3(-2147483648, -2147483648, -2147483648);
+      setSpawPos.blockPosition = setSpawPos.position;
+      setSpawPos.type = SpawnType.PLAYER_SPAWN;
+      await session.getConnection().sendDataPacket(setSpawPos);
 
-            await session.sendTime(world.getTicks());
+      await session.sendTime(world.getTicks());
 
-            // TODO: set difficulty packet
-            // TODO: set commands enabled packet
+      // TODO: set difficulty packet
+      // TODO: set commands enabled packet
 
-            await session.sendSettings();
-            await session.sendAbilities();
+      await session.sendSettings();
+      await session.sendAbilities();
 
-            // TODO: game rules changed packet
+      // TODO: game rules changed packet
 
-            await session.sendPlayerList();
+      await session.sendPlayerList();
 
-            await session.getConnection().sendDataPacket(new BiomeDefinitionListPacket());
-            await session.getConnection().sendDataPacket(new AvailableActorIdentifiersPacket());
+      await session
+        .getConnection()
+        .sendDataPacket(new BiomeDefinitionListPacket());
+      await session
+        .getConnection()
+        .sendDataPacket(new AvailableActorIdentifiersPacket());
 
-            // TODO: player fog packet
+      // TODO: player fog packet
 
-            await session.sendAttributes(player.getAttributeManager().getDefaults());
+      await session.sendAttributes(player.getAttributeManager().getDefaults());
 
-            await session.sendMetadata();
+      await session.sendMetadata();
 
-            await session.sendCreativeContents();
+      await session.sendCreativeContents();
 
-            // Some packets...
+      // Some packets...
 
-            // TODO: inventory crafting data
+      // TODO: inventory crafting data
 
-            // TODO: available commands
+      // TODO: available commands
 
-            const respawnPacket = new RespawnPacket();
-            respawnPacket.state = RespawnState.SERVER_SEARCHING_FOR_SPAWN;
-            respawnPacket.position = await world.getSpawnPosition();
-            respawnPacket.runtimeEntityId = player.getRuntimeId();
-            await session.getConnection().sendDataPacket(respawnPacket);
+      const respawnPacket = new RespawnPacket();
+      respawnPacket.state = RespawnState.SERVER_SEARCHING_FOR_SPAWN;
+      respawnPacket.position = await world.getSpawnPosition();
+      respawnPacket.runtimeEntityId = player.getRuntimeId();
+      await session.getConnection().sendDataPacket(respawnPacket);
 
-            respawnPacket.state = RespawnState.SERVER_READY_TO_SPAWN;
-            await session.getConnection().sendDataPacket(respawnPacket);
-            await session.getConnection().sendDataPacket(respawnPacket);
+      respawnPacket.state = RespawnState.SERVER_READY_TO_SPAWN;
+      await session.getConnection().sendDataPacket(respawnPacket);
+      await session.getConnection().sendDataPacket(respawnPacket);
 
-            /// TODO: general handler refactor ///
+      /// TODO: general handler refactor ///
 
-            // Sent to let know the client saved chunks
-            // TODO
-            session
-                .getPlayer()
-                .sendInitialSpawnChunks()
-                .then(
-                    async () => {
-                        // todo: set health packet
-                        await session.sendPlayStatus(PlayStatusType.PlayerSpawn);
-                    },
-                    () =>
-                        server
-                            .getLogger()
-                            ?.error(
-                                `Failed to spawn player[${session.getConnection().getRakNetSession().getAddress()}]`
-                            )
-                )
-                .finally(async () => {
-                    // Summon player(s) & entities
-                    await Promise.all([
-                        server
-                            .getSessionManager()
-                            .getAllPlayers()
-                            .filter((p) => p !== player)
-                            .map(async (p) => {
-                                await p.getNetworkSession().sendSpawn(player);
-                                await session.sendSpawn(p);
-                            }),
-                        player
-                            .getWorld()
-                            .getEntities()
-                            .filter((e) => !e.isPlayer())
-                            .map(async (entity) => entity.sendSpawn(player))
-                    ]);
-                });
-
+      // Sent to let know the client saved chunks
+      // TODO
+      session
+        .getPlayer()
+        .sendInitialSpawnChunks()
+        .then(
+          async () => {
+            // todo: set health packet
+            await session.sendPlayStatus(PlayStatusType.PlayerSpawn);
+          },
+          () =>
             server
-                .getLogger()
-                ?.info(
-                    `§b${player.getName()}§f is attempting to join with id §b${player.getRuntimeId()}§f from ${player
-                        .getAddress()
-                        .getAddress()}:${player.getAddress().getPort()}`,
-                    'Handler/ResourcePackResponseHandler'
-                );
+              .getLogger()
+              ?.error(
+                `Failed to spawn player[${session.getConnection().getRakNetSession().getAddress()}]`,
+              ),
+        )
+        .finally(async () => {
+          // Summon player(s) & entities
+          await Promise.all([
+            server
+              .getSessionManager()
+              .getAllPlayers()
+              .filter((p) => p !== player)
+              .map(async (p) => {
+                await p.getNetworkSession().sendSpawn(player);
+                await session.sendSpawn(p);
+              }),
+            player
+              .getWorld()
+              .getEntities()
+              .filter((e) => !e.isPlayer())
+              .map(async (entity) => entity.sendSpawn(player)),
+          ]);
+        });
 
-            player.setNameTag(player.getName());
-            // TODO: always visible nametag
+      server
+        .getLogger()
+        ?.info(
+          `§b${player.getName()}§f is attempting to join with id §b${player.getRuntimeId()}§f from ${player
+            .getAddress()
+            .getAddress()}:${player.getAddress().getPort()}`,
+          "Handler/ResourcePackResponseHandler",
+        );
 
-            // TODO: fix await player.getConnection().sendInventory();
+      player.setNameTag(player.getName());
+      // TODO: always visible nametag
 
-            // Announce connection
-            const chatSpawnEvent = new ChatEvent(
-                new Chat(
-                    server.getConsole(),
-                    `§e%multiplayer.player.joined`, // TODO: enum of client hardcoded messages
-                    [player.getName()],
-                    true,
-                    '*.everyone',
-                    ChatType.TRANSLATION
-                )
-            );
-            await server.getEventManager().emit('chat', chatSpawnEvent);
-        }
+      // TODO: fix await player.getConnection().sendInventory();
+
+      // Announce connection
+      const chatSpawnEvent = new ChatEvent(
+        new Chat(
+          server.getConsole(),
+          `§e%multiplayer.player.joined`, // TODO: enum of client hardcoded messages
+          [player.getName()],
+          true,
+          "*.everyone",
+          ChatType.TRANSLATION,
+        ),
+      );
+      await server.getEventManager().emit("chat", chatSpawnEvent);
     }
+  }
 }
