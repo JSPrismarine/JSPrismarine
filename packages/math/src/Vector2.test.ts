1394138528e95d983f7923ba520dac15ef4f8b25
Filiph Siitam Sandström <filiph@nordcom.io>
2024-04-17 03:16:22 +0200

    Meta: Add a few tests.

--- packages/math/src/Vector2.test.ts
+++ packages/math/src/Vector2.test.ts
@@ -1,33 +1,26 @@
 import { describe, expect, it } from 'vitest';
 
-import { Vector3 } from './Vector3';
+import { Vector2 } from './Vector2';
 
 describe('math', () => {
     describe('Vector3', () => {
-        const vector = new Vector3(1.5, 0, 2.75);
+        const vector = new Vector2(1.5, 2.75);
 
         it('should retrieve values correctly', () => {
             expect(vector.getX()).toBe(1.5);
-            expect(vector.getY()).toBe(0);
             expect(vector.getZ()).toBe(2.75);
         });
 
-        it('should set and get Y coordinate correctly', () => {
-            vector.setY(10);
-            expect(vector.getY()).toBe(10);
-        });
-
         it('should floor the vector correctly', () => {
-            const flooredVector = new Vector3(1.5, 0, 2.75).floor();
+            const flooredVector = new Vector2(1.5, 2.75).floor();
             expect(flooredVector.getX()).toBe(1);
-            expect(flooredVector.getY()).toBe(0);
             expect(flooredVector.getZ()).toBe(2);
         });
 
         it('should compare two vectors correctly', () => {
-            const vector1 = new Vector3(1, 2, 3);
-            const vector2 = new Vector3(1, 2, 3);
-            const vector3 = new Vector3(4, 5, 6);
+            const vector1 = new Vector2(1, 3);
+            const vector2 = new Vector2(1, 3);
+            const vector3 = new Vector2(4, 6);
 
             expect(vector1.equals(vector2)).toBe(true);
             expect(vector1.equals(vector3)).toBe(false);
