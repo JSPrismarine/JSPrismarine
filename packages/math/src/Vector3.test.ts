f5ad325b213b75c50016b17f70f36785f694282a
Filiph Siitam Sandström <filiph@nordcom.io>
2024-04-17 02:42:15 +0200

    CI: Fix flakey test.

--- packages/math/src/Vector3.test.ts
+++ packages/math/src/Vector3.test.ts
@@ -1,6 +1,6 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
-import Vector3 from './Vector3';
+import { Vector3 } from './Vector3';
 
 describe('math', () => {
     describe('Vector3', () => {
