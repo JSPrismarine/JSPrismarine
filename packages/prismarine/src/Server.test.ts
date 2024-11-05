fb0154f3e65a5ed08351ce1a17ff3952e6fc12d6
Filiph Siitam Sandström <filiph@nordcom.io>
2024-11-05 01:47:00 +0100

    Prismarine: Improve server tests.

--- packages/prismarine/src/Server.test.ts
+++ packages/prismarine/src/Server.test.ts
@@ -4,74 +4,97 @@ import { Logger } from '@jsprismarine/logger';
 import Server from './Server';
 
 describe('Prismarine', () => {
-    it('server to start & exit properly', async () => {
-        const getRandomInt = (min: number, max: number) => {
-            min = Math.ceil(min);
-            max = Math.floor(max);
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        };
-
-        const logger = new Logger();
-        const prismarine = new Server({
-            logger,
-            config: new (class DebugConfig {
-                public enable() {}
-                public disable() {}
-
-                public getPort() {
-                    return 19199;
-                }
-
-                public getServerIp() {
-                    return '0.0.0.0';
-                }
-
-                public getLevelName() {
-                    return '';
-                }
-
-                public getWorlds() {
-                    return {};
-                }
-
-                public getMaxPlayers() {
-                    return 1;
-                }
-
-                public getGamemode() {
-                    return 1;
-                }
-
-                public getMotd() {
-                    return 'CI';
-                }
-
-                public getViewDistance() {
-                    return 4;
-                }
-
-                public getOnlineMode() {
-                    return false;
-                }
-
-                public getEnableEval() {
-                    return false;
-                }
-
-                public getPacketCompressionLevel() {
-                    return 7;
-                }
-
-                public getEnableTicking() {
-                    return false;
-                }
-            })() as any
+    describe('Server', () => {
+        vi.mock('@jsprismarine/raknet', async (importActual) => {
+            return {
+                ...(((await importActual()) as any) || {}),
+                RakNetListener: vi.fn().mockImplementation(() => ({
+                    start: vi.fn(),
+                    on: vi.fn()
+                }))
+            };
         });
 
-        const mockExit = vi.spyOn(prismarine, 'shutdown').mockImplementation((() => {}) as any);
+        const config = new (class DebugConfig {
+            public enable() {}
+            public disable() {}
 
-        await prismarine.bootstrap('0.0.0.0', getRandomInt(46000, 49999));
-        await prismarine.shutdown();
-        expect(mockExit).toBeCalledTimes(1);
+            public getPort() {
+                return 19199;
+            }
+
+            public getServerIp() {
+                return '0.0.0.0';
+            }
+
+            public getLevelName() {
+                return '';
+            }
+
+            public getWorlds() {
+                return {};
+            }
+
+            public getMaxPlayers() {
+                return 1;
+            }
+
+            public getGamemode() {
+                return 1;
+            }
+
+            public getMotd() {
+                return 'CI';
+            }
+
+            public getViewDistance() {
+                return 4;
+            }
+
+            public getOnlineMode() {
+                return false;
+            }
+
+            public getEnableEval() {
+                return false;
+            }
+
+            public getEnableTicking() {
+                return false;
+            }
+
+            public getPacketCompressionLevel() {
+                return 7;
+            }
+        })() as any;
+
+        it('starts and stops without crashing', async () => {
+            const logger = new Logger();
+            const prismarine = new Server({
+                logger,
+                config
+            });
+
+            const mockExit = vi.spyOn(prismarine, 'shutdown').mockImplementation((() => {}) as any);
+
+            await prismarine.bootstrap('0.0.0.0', 12345);
+            await expect(() => prismarine.shutdown()).not.toThrow();
+            expect(mockExit).toBeCalledTimes(1);
+        });
+
+        it('starts and stops without crashing in headless mode', async () => {
+            const logger = new Logger();
+            const prismarine = new Server({
+                logger,
+                config,
+                headless: true
+            });
+
+            const mockExit = vi.spyOn(prismarine, 'shutdown').mockImplementation((() => {}) as any);
+
+            await prismarine.bootstrap('0.0.0.0', 12345);
+            await expect(() => prismarine.shutdown()).not.toThrow();
+            expect(mockExit).toBeCalledTimes(1);
+        });
     });
 });
